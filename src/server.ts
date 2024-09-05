6f62ecb6dcdeb2683eb6f03e04467773dc297e65
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2024-09-05 19:26:42 +0100

    Add database connection

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { checkServerHealth } from './utils/health-check.util';
 import { preMiddleware } from './middleware/pre.middleware';
 import { socketSetup } from './configs/socket.config';
 import { PORT } from './configs/constants';
+import connectToMongo from './configs/database.configs';
 
 const app = express();
 preMiddleware(app);
@@ -13,6 +14,7 @@ socketSetup(server);
 
 setInterval(checkServerHealth, 15 * 60 * 1000);
 
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
+  await connectToMongo();
   console.log(`Server is running on port ${PORT}`);
 });
