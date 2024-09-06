20c6e6f434f5f73dd20c315548cd5ac164ed50d3
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2024-09-06 16:55:03 +0100

    Add emBot sponsorship ad broadcast
    Sends a new ad every 2 minutes.

--- src/services/ai.service.ts
+++ src/services/ai.service.ts
@@ -1,9 +1,10 @@
+import { ISponsorshipAd } from '../interfaces';
 import { SponsorshipAdModel } from '../models/sponsorship-ad.model';
 import { Types } from 'mongoose';
 
 let sentAds: Types.ObjectId[] = []; // Store ObjectId instead of strings
 
-export async function getRandomSponsorAd() {
+export async function getRandomSponsorAd(): Promise<string | boolean> {
   while (true) {
     // Use a loop instead of recursion
     let ad = await SponsorshipAdModel.aggregate([
@@ -19,7 +20,9 @@ export async function getRandomSponsorAd() {
 
     if (ad.length > 0) {
       sentAds.push(ad[0]._id);
-      return ad[0];
+      
+      // return ad[0];
+      return `${ad[0].title}\n${ad[0].description}`;
     }
 
     // If no ad is found, reset the sentAds array and loop again
